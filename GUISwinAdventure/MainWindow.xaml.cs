8cfc49e5fed25cf6cc62e30daadedd24a815b5fb
Hoang Cuong Nguyen <76920754+hoangcuongnguyen2001@users.noreply.github.com>
2021-03-02 14:37:23 +1100

    Add files via upload

---
+++ GUISwinAdventure/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using SwinAdventure;
+
+namespace GUISwinAdventure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        // help from Jimmy Trac.
+        ProgramReference _newProgram;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _newProgram = new ProgramReference();
+            Console.Text = _newProgram.Output;
+        }
+        private void EnterClick(object sender, RoutedEventArgs e)
+        {
+            Console.Text = "\n" + _newProgram.InputCommand(commandBox.Text);
+            commandBox.Clear();
+        }
+
+        private void TextChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
